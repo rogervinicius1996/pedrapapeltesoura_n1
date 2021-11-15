f9bac12ec00f1e564d0eedcfc03c1f596f637c88
Roger Vinicius <rogervinicius1996@gmail.com>
2021-11-15 14:50:32 -0300

    Feat: modifica identação e adiciona escolha do jogador e do pc Arquivos modificados: 	   bin/App.class 	   bin/Ptp.class 	   src/App.java 	   src/Ptp.java

--- src/App.java
+++ src/App.java
@@ -1,22 +1,20 @@
 import java.lang.Math;
 import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
         Scanner pl = new Scanner(System.in);
         Ptp ptp = new Ptp();
-        for (int c = 1 ; c != 0; c = pl.nextInt()){
-        System.out.println("digite 1 para PEDRA");
-        System.out.println("digite 2 para TESOURA");
-        System.out.println("digite 3 para PAPEL");
-        int jogador = pl.nextInt(); 
-        int   pc  = (int) Math.floor(Math.random()* 3) + 1;
-        System.out.println(ptp.jogo(jogador, pc));
-        System.out.println("digite 0 para encerrar ou outro numero para continuar");
+        for (int c = 1; c != 0; c = pl.nextInt()) {
+            System.out.println("digite 1 para PEDRA");
+            System.out.println("digite 2 para TESOURA");
+            System.out.println("digite 3 para PAPEL");
+            int jogador = pl.nextInt();
+            int pc = (int) Math.floor(Math.random() * 3) + 1;
+            System.out.println(ptp.jogo(jogador, pc));
+            System.out.println("digite 0 para encerrar ou outro numero para continuar");
         }
-        
-    
 
-    
-        }
-    
+    }
+
 }
--- src/Ptp.java
+++ src/Ptp.java
@@ -1,25 +1,19 @@
 public class Ptp {
-    public String jogo(int jogador, int pc){
-        if (jogador == pc){
+    public String jogo(int jogador, int pc) {
+        if (jogador == pc) {
             return "EMPATOU - jogue novamente";
-        }    
-        else if (jogador == 1 && pc == 2){
-            return "Pedra quebra Tesoura --Você ganhou!!";
-        }
-        else if (jogador == 1 && pc == 3){
-            return "Papel embrulha Pedra -- Você perdeu!!";
-        }
-        else if (jogador == 2 && pc == 1 ){
-            return "Pedra quebra Tesoura -- Você perdeu !!";
-        }
-        else if (jogador ==2 && pc == 3){
-            return "Tesoura corta Papel -- Você ganhou!!";
-        }
-        else if (jogador == 3 && pc == 1){
-            return "Papel embrulha Pedra -- Você ganhou!!";
-        }
-        else {
-            return "Tesoura corta Papel -- Você perdeu!!";
+        } else if (jogador == 1 && pc == 2) {
+            return "Pedra(você) quebra Tesoura(pc) --Você ganhou!!";
+        } else if (jogador == 1 && pc == 3) {
+            return "Papel(pc) embrulha Pedra(você) -- Você perdeu!!";
+        } else if (jogador == 2 && pc == 1) {
+            return "Pedra(pc) quebra Tesoura(você) -- Você perdeu !!";
+        } else if (jogador == 2 && pc == 3) {
+            return "Tesoura(você) corta Papel(pc) -- Você ganhou!!";
+        } else if (jogador == 3 && pc == 1) {
+            return "Papel(você) embrulha Pedra(pc) -- Você ganhou!!";
+        } else {
+            return "Tesoura(pc) corta Papel(você) -- Você perdeu!!";
         }
     }
 }
